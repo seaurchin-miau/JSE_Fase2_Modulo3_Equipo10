d321f695c4ed69dcad5363c1915a3c44e4674f9a
Anna Luz Gómez <110350110+seaurchin-miau@users.noreply.github.com>
2023-05-02 08:02:19 -0600

    Create SistemaMedicion.java

---
+++ Sesion4/work/Ejemplos/Ejemplo1/SistemaMedicion.java
@@ -0,0 +1,15 @@
+package Ejemplo1;
+
+import java.util.concurrent.TimeUnit;
+
+public class SistemaMedicion {
+
+    public double leer(int id) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return id*Math.random();
+    }
+}
