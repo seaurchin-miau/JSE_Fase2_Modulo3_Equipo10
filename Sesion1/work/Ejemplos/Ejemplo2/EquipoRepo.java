e75572cd5fdd08206b8475d0be1a040f4f4a95cf
Anna Luz Gómez <110350110+seaurchin-miau@users.noreply.github.com>
2023-05-02 11:48:38 -0600

    Create EquipoRepo.java

---
+++ Sesion1/work/Ejemplos/Ejemplo2/EquipoRepo.java
@@ -0,0 +1,7 @@
+package org.bedu.javase2.ejemplo2.repositories;
+
+import org.bedu.javase2.ejemplo2.models.Equipo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EquipoRepo extends CrudRepository<Equipo, Long> {
+}
