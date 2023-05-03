56d04ee73f1cfa5dd4ecfc6b4505198072b8d5e9
Anna Luz Gómez <110350110+seaurchin-miau@users.noreply.github.com>
2023-05-02 21:42:50 -0600

    Reto1 listo

--- Sesion4/work/Retos/Reto1/CadenaRestaurantes.java
+++ Sesion4/work/Retos/Reto1/CadenaRestaurantes.java
@@ -21,16 +21,4 @@ public class CadenaRestaurantes {
             }
 
             return rnd.nextInt(40000) + 10000;
-        }
-    
-    private static void obtenerSuma(Stream<Integer> ids) {
-    LocalTime inicio = LocalTime.now(); 
-    double suma = ids.mapToDouble(id -> new CadenaRestaurantes()
-                    .obtenerGananciasFranquicia(id))
-            .sum();
-    System.out.printf("Ganancias totales: $%5.2f%n", suma);
-    Duration tiempo = Duration.between(inicio, LocalTime.now());    
-    System.out.println((Math.round(suma * 100.) / 100.) + " en " + tiempo.toMillis() + "ms"); 
-    }
-    
- }
+  }
