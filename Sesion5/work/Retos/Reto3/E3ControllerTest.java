32faa98bf287f2998b1f24d50e7c0c2fa0a55141
Anna Luz Gómez <110350110+seaurchin-miau@users.noreply.github.com>
2023-05-02 21:24:40 -0600

    Update E3ControllerTest.java

--- Sesion5/work/Retos/Reto3/E3ControllerTest.java
+++ Sesion5/work/Retos/Reto3/E3ControllerTest.java
@@ -1,15 +1,30 @@
 package Ejemplo3;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
 
-@Test
-@DisplayName("Reto3")
-void reto3() {
-   E3Controller controller = new E3Controller();
 
-   controller.buscarPorNombre(NOMBRE_QUERY)
-      .subscribe(x -> {
-         assertThat(x.getNomCom()).isEqualTo(NOMBRE_COMPLETO);
-         assertThat(x.getTel()).isEqualTo(TELEFONO);
-      });
+class E3ControllerTest { 
+   
+     private static final List<String> NOMBRES = Arrays.asList(new String[]{"Romo Përez, Juan", "Sánchez García, Diana", "Rodríguez Soto, Sebastian"});
+     private static final String NOMBRE_QUERY = "DIA";
+     private static final String NOM_COM = "Diana Sánchez García"
+     private static final String TEL = "5550000002"
+   
+   @Test
+   @DisplayName("Reto3")
+   void reto3() {
+      E3Controller controller = new E3Controller();
+
+      controller.buscarPorNombre(NOMBRE_QUERY)
+         .subscribe(x -> {
+            assertThat(x.getNomCom()).isEqualTo(NOMBRE_COMPLETO);
+            assertThat(x.getTel()).isEqualTo(TELEFONO);
+         });
+   }
+
+
 }
