85b38cf5d926166ec6e1f538496ca1d6e6d2ffb8
Anna Luz Gómez <110350110+seaurchin-miau@users.noreply.github.com>
2023-05-02 18:50:44 -0600

    Reto2 listo

---
+++ Sesion1/work/Retos/Reto2/PartidoRepo.java
@@ -0,0 +1,7 @@
+package org.bedu.javase2.repositories;
+
+import org.bedu.javase2.models.Partido;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartidoRepo extends CrudRepository<Partido,Long> {
+}
